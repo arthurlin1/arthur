8bd7172d32c981071904c4d216b983917f244d79
arthurlin1 <54224985+arthurlin1@users.noreply.github.com>
2019-08-18 11:56:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ def home():
   return render_template("index.html", cat=fill)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port='3456')
+  app.run(debug=True, host='0.0.0.0', port='3000')
